526afafddbfde83df793d1507bfb590f282f39b3
chandu7077 <chanduburre7@gmail.com>
2021-02-09 18:59:07 +0530

    handson2 completed

---
+++ Handson2/src/App.js
@@ -0,0 +1,14 @@
+import Contact from "../src/Components/Contact";
+import Home from "../src/Components/Home";
+import About from "../src/Components/About";
+function App() {
+  return (
+    <div className="container">
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
